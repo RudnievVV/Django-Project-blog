cdc2a6428f7a9f7c6243341768ccd312d4661277
Vladyslav Rudniev <RudnievVV@gmail.com>
2019-10-15 18:13:57 +0300

    minor

--- django_project/blog/templatetags/custom_tags.py
+++ django_project/blog/templatetags/custom_tags.py
@@ -11,5 +11,6 @@ def latest_posts():
     latest_posts_html = ''
     latest_posts_list = Post.objects.all().order_by('-date_posted')[:3]
     for post in latest_posts_list:
-        latest_posts_html += f"<br><a href='/post/{post.id}/'>{post.title}</a>"
+        latest_posts_html += f"""<p><a href='/post/{post.id}/'>{post.title}</a>
+                             <br><small class='text-muted'>Posted by <strong>{post.author}</strong></small></p>"""
     return mark_safe(latest_posts_html)
