47d0350cde408e6b0132c92563f02bf6de9403cc
Vladyslav Rudniev <RudnievVV@gmail.com>
2019-11-22 19:19:08 +0200

    categories-nav-search

--- django_project/blog/static/blog/javascript.js
+++ django_project/blog/static/blog/javascript.js
@@ -20,19 +20,19 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-        $("#categories_dropbtn_search").on("keyup", function(){
-            var input, filter, ul, li, a, i;
-            input = document.getElementById("categories_dropbtn_search");
-            filter = input.value.toUpperCase();
-            div = document.getElementById("nav_dropdown_categories");
-            a = div.getElementsByTagName("a");
-            for (i = 0; i < a.length; i++) {
-                txtValue = a[i].textContent || a[i].innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    a[i].style.display = "";
-                } else {
-                    a[i].style.display = "none";
-                };
-          };
-        });
+    $("#categories_dropbtn_search").on("keyup", function(){
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("categories_dropbtn_search");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("nav_dropdown_categories");
+        a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+            txtValue = a[i].textContent || a[i].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                a[i].style.display = "";
+            } else {
+                a[i].style.display = "none";
+            };
+      };
+    });
 });
